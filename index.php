085a5c4a76cd7869c0e60f67f3664bddd54c0895
pushkar makarand gadgil <pg395@afsconnect1.njit.edu>
2017-11-05 20:31:51 -0500

    added myPDO assignment

--- index.php
+++ index.php
@@ -1,38 +1,79 @@
 <?php
-$hostname = "sql.njit.edu";
-$username = "pg395";
-$password = "r4Matf7xW";
-$conn = NULL;
-try
-  {
-      $conn = new PDO("mysql:host=$hostname;dbname=pg395",
-    $username, $password);
-  //$myHandle = new PDO("sql.njit.edu")
-//	$myHandle->myAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }   catch(PDOException $pe)
-      {
-		echo "server gave error as:".$pe->getMessage()."</br>";
-		die();
-      }
-	  
-	  
-echo 'Connection Successful..!! </br>';	 
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
 
+define('DATABASE', 'pg395');
+define('USERNAME', 'pg395');
+define('PASSWORD', 'r4Matf7xW');
+define('CONNECTION', 'sql.njit.edu');
 
-$myQuerry = $conn->prepare('SELECT * FROM accounts where id<6');
-$myQuerry->execute();
+class dbConn{
 
-$r = $myQuerry->fetchAll();
-echo "Total number of results are: ".count($r)."</br>";
-echo "<table border=\"1\"><tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>";
-foreach($r as $temp){
-//echo "<span>".$temp["id"]."</span>&nbsp &nbsp <span>".$temp["fname"]."</span>";
-echo "<tr><td>".$temp["id"]."</td><td>".$temp["email"]."</td><td>".$temp["fname"]."</td><td>".$temp["lname"]."</td><td>".$temp["phone"]."</td><td>".$temp["birthday"]."</td><td>".$temp["gender"]."</td><td>".$temp["password"]."</td></tr>";
-//echo 'yes';
+
+    protected static $dbConnection;
+
+
+    private function __construct() {
+
+        try {
+
+            self::$dbConnection = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$dbConnection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $excep) {
+
+            echo "The server gave error as: ".$excep->getMessage()."</br>";
+        }
+
+    }
+
+
+    public static function getConnection() {
+
+
+        if (!self::$dbConnection) {
+          new dbConn();
+        }
+return self::$dbConnection;
+    }
 }
-//echo '<pre>', print_r($r), '</pre>';
+
+
+class collection {
+
+
+    static public function getRecords() {
+
+        $dbConnection = dbConn::getConnection();
+        echo 'Connection Successful </br>';
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName.' where id < 6';
+        $statement = $dbConnection->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $records =  $statement->fetchAll();
+        return $records;
+    }
+}
 
 
 
+class accounts extends collection {
+    protected static $modelName = 'account';
+}
+
+class account {}
+
+
+$accountRecords = accounts::getRecords();
+
+echo "Total number of results are: ".count($accountRecords)."</br>";
+echo "<table border=\"1\"><tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>";
+foreach($accountRecords as $tempRecord){
+
+echo "<tr><td>".$tempRecord->id."</td><td>".$tempRecord->email."</td><td>".$tempRecord->fname."</td><td>".$tempRecord->lname."</td><td>".$tempRecord->phone."</td><td>".$tempRecord->birthday."</td><td>".$tempRecord->gender."</td><td>".$tempRecord->password."</td></tr>";
+}
 ?>
